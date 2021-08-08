3a281b2bc117d211b18ed93f2df57b9d3aebfaf2
= <=>
2021-08-09 00:48:37 +0200

    atempting to load tank

--- threejs/sketch.js
+++ threejs/sketch.js
@@ -1,4 +1,5 @@
 import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/build/three.module.js';
+import { GLTFLoader } from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/loaders/GLTFLoader.js';
 
 const canvas = document.querySelector('#canvas');
 const renderer = new THREE.WebGLRenderer({canvas, antialias: true});
@@ -6,25 +7,49 @@ const renderer = new THREE.WebGLRenderer({canvas, antialias: true});
 const cameraP = {fov: 90, aspect: 2, near: 0.1, far: 30};
 const camera = new THREE.PerspectiveCamera(cameraP.fov, cameraP.aspect, cameraP.near, cameraP.far);
 
-camera.position.z = 12
+camera.position.z = 1
 
 const scene = new THREE.Scene();
 
 
-const geometry = new THREE.BoxGeometry(10, 10, 10);
+// const geometry = new THREE.BoxGeometry(10, 10, 10);
 
-const material = new THREE.MeshBasicMaterial({color: 0x44aa88});  // greenish blue
+// const material = new THREE.MeshBasicMaterial({color: 0x44aa88});  // greenish blue
 
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// const cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
+
+
+const loader = new GLTFLoader();
+
+loader.load(
+    '../Resources/Blender/tank.glb',
+    
+    function ( gltf ) {
+        const material = new THREE.MeshBasicMaterial({color: 0x44aa88});  // greenish blue
+
+        scene.add( gltf.scene );
+        console.log("Tank loaded");
+    },
+    function ( xhr ) { // called while loading is progressing
+        console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+    },
+    function ( error ) { // error
+        console.error( error );
+    } 
+);
 
 
 function animate(time) {
-    time *= 0.001;  // seconds
+    // time *= 0.001;  // seconds
+    // time *= 0.01;  // seconds
   
-    cube.rotation.x = Math.cos(time * 0.5);
-    cube.rotation.y = time * 0.5;
+    // cube.rotation.x = Math.cos(time * 0.5);
+    // cube.rotation.y = time * 0.5;
     // cube.rotation.y = time * 1;
+
+    camera.position.z += time;
+    // console.log(camera.position.z)
   
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
