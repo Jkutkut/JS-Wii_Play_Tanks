2c23b1f911ec0ba796111d289a5676043c28b7c0
= <=>
2021-08-09 23:58:46 +0200

    now tank mesh is stored on the Tank class

--- sketch.js
+++ sketch.js
@@ -1,20 +1,13 @@
 var scene;
 
-var tank = {
-    head: null,
-    gunTip: null,
-    body: null,
-    tires: null
-};
-
 function displayTank() {
-    scene.add(tank.head);
-    scene.add(tank.gunTip);
-    scene.add(tank.body);
-    scene.add(tank.tires);
+    scene.add(Tank.MESH.head);
+    scene.add(Tank.MESH.body);
 };
 
-
+/**
+ * This function is executed in every frame of the scene.
+ */
 function gameTick() {
-    
+
 }
